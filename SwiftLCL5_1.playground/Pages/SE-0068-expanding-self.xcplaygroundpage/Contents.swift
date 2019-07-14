a040e29ee6489c9951f37887b27f077e775cb902
Nicholas Maccharoli <nicko@screaming-cactus.com>
2019-07-14 18:12:17 +0900

    add in static subscript talk

--- SwiftLCL5_1.playground/Pages/SE-0068-expanding-self.xcplaygroundpage/Contents.swift
+++ SwiftLCL5_1.playground/Pages/SE-0068-expanding-self.xcplaygroundpage/Contents.swift
@@ -1,7 +1,23 @@
 //: [Previous](@previous)
 
-import Foundation
+/*:
+ # `Self` & You
+`Self` can now be used to refer to the dynamic type when used inside of `class`, `enum` and `struct`s.
+ This will come in very handy when using generics since instead of typing `Type<T>` everytime one wishes to reference the
+ dynamic type of self they can just type `Self` (with a big S )
+ */
 
-var str = "Hello, playground"
+struct Foo<T> {
+    
+    //: This is the same as the copy below, except more straightforward using only the keyword `Self`
+    func copy() -> Self {
+        return self
+    }
+    
+    //: This is the same as the above except we have to repeat ourselves by specifying the generic parameter again
+    func _copy() -> Foo<T> {
+        return self
+    }
+}
 
 //: [Next](@next)
--- SwiftLCL5_1.playground/Pages/SE-0254-Static-and-class-subscripts.xcplaygroundpage/Contents.swift
+++ SwiftLCL5_1.playground/Pages/SE-0254-Static-and-class-subscripts.xcplaygroundpage/Contents.swift
@@ -1,16 +1,58 @@
 //: [Previous](@previous)
 /*:
- # Static and class subscripts SE-0254
+ ## Static and class subscripts SE-0254
  
- ## [Click Here for Proposal](https://github.com/apple/swift-evolution/blob/master/proposals/0254-static-subscripts.md)
+ ### [Click Here for Proposal](https://github.com/apple/swift-evolution/blob/master/proposals/0254-static-subscripts.md)
+ 
+ This proposal extends the usage of subscripts with subscripting into static or class types.
+ The way you do it does not change from before, just as before you could define a type with subscripting capabilities like so:
  */
 
-import Foundation
+struct EmployeeTable {
+    struct Employee {
+        let name: String
+        let division: String
+    }
+    private let employees = [
+        Employee(name: "Bob Lazar", division: "S4"),
+        Employee(name: "Steve Smith", division: "Groom Lake"),
+        Employee(name: "Mike Thigpen", division: "Los Alamos")
+    ]
+    
+    subscript(_ name: String) -> Employee? {
+        return employees.filter { $0.name.lowercased() == name.lowercased() }.first
+    }
+}
 
-var str = "Hello, playground"
+let et = EmployeeTable()
+let bob = et["bob lazar"]
+bob?.division // will return S4
 
 
+/*:
+ Now that static subscripts are allowed the code above could be run without the extra step of creating an object
+ because the subscript could be called on the type directly!
+ */
 
+struct EmployeeTable2 {
+    struct Employee {
+        let name: String
+        let division: String
+    }
+    static private let employees = [
+        Employee(name: "Bob Lazar", division: "S4"),
+        Employee(name: "Steve Smith", division: "Groom Lake"),
+        Employee(name: "Mike Thigpen", division: "Los Alamos")
+    ]
+    
+    public static subscript(_ name: String) -> Employee? {
+        get {
+            return employees.filter { $0.name.lowercased() == name.lowercased() }.first
+        }
+    }
+}
+
+EmployeeTable2["bob lazar"]?.division
 
 
 
